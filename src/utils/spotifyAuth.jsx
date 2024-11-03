1b7c0f85c7b62db651fb04488b73bf72898b4b6a
Le-LongTien <tienlec12@gmail.com>
2024-11-03 19:21:03 +0700

    sadfa

--- src/utils/spotifyAuth.jsx
+++ src/utils/spotifyAuth.jsx
@@ -1,5 +1,5 @@
-const client_id = 'f360dca477a244558bdc0ae00b0c0217';  // Thay YOUR_CLIENT_ID bằng client ID thực tế
-const redirect_uri = 'http://localhost:5173/';  // Thay YOUR_REDIRECT_URI bằng URI thực tế
+const client_id = 'd2b2f93d92c84b51a07a6c9c762e4ab6d2b2f93d92c84b51a07a6c9c762e4ab6';  // Thay YOUR_CLIENT_ID bằng client ID thực tế
+const redirect_uri = 'https://spotify-eight-ochre.vercel.app/';  // Thay YOUR_REDIRECT_URI bằng URI thực tế
 const scopes = 'user-read-private user-read-email user-library-read';
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
